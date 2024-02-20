855b8b1ed72fbb5e5bc9fe3aa181edbfd9edc4c5
pelaezluis <luispelaezbedoya@gmail.com>
2024-02-20 01:21:41 -0500

    change in pages

--- src/pages/BandInfo.jsx
+++ src/pages/BandInfo.jsx
@@ -1,10 +1,9 @@
 import './BandInfo.css'
-import { Header } from './Header'
+
 export const BandInfo = ({title, description}) => {
     const location = title.split(' ') > 1 ? title.split(' ').join('').toLowerCase() : title.toLowerCase()
     return (
         <>
-            <Header />
             <div className="band-info">
                 <h1 id={location}>{title}</h1>
                 {description}
