5b61c65ad498bd09a479a6777d38f1e115896c9f
pelaezluis <luispelaezbedoya@gmail.com>
2024-02-26 16:28:10 -0500

    all bands basic

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -9,15 +9,15 @@ export const SocialMedia = ({instagram_url, facebook_url}) => {
             <p>
                 Para más información escribenos a whatsapp (clic en el icono)
             </p>
-            <div className='social-media'>
+            <div className='social-media-wa'>
                 <a href="https://wa.me/+573166514295" target='blank'><img src={whatsapp} alt="" /></a>
             </div>
             <p>
                 O visitanos en Instagram y Facebook como @rockbitarockband
             </p>
             <div className='social-media'>
-                <a href={instagram_url} target='blank_'><img src={instagram} alt="" /></a>
-                <a href={facebook_url} target='blank'><img src={facebook} alt="" /></a>
+                <a href={instagram_url} target='blank_'><img src={instagram} alt="instagram" /></a>
+                <a href={facebook_url} target='blank_'><img src={facebook} alt="facebook" /></a>
             </div>
         </>
     )
